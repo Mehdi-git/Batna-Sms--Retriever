f32b7a93fa042b45a5a5c3fa3e98d8af3ad91a7b
bakhtiari <mbwithoutd@gmail.com>
2020-10-28 09:41:14 +0330

    add AppSignatureHelper to library

--- app/src/main/java/ir/batna/batnasmsretriever/MainActivity.java
+++ app/src/main/java/ir/batna/batnasmsretriever/MainActivity.java
@@ -1,7 +1,6 @@
 package ir.batna.batnasmsretriever;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
     }
 }

3de10fe5b549cae2ada05f8efa3a3e128bd21e53
bakhtiari <mbwithoutd@gmail.com>
2020-11-02 10:29:42 +0330

    Some change

--- SmsRetrieverlibrary/src/main/java/ir/batna/smsretrieverlibrary/SmsRetriever.java
+++ SmsRetrieverlibrary/src/main/java/ir/batna/smsretrieverlibrary/SmsRetriever.java
@@ -3,7 +3,6 @@ package ir.batna.smsretrieverlibrary;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 import ir.batna.smsretrieverlibrary.utils.Constant;
 

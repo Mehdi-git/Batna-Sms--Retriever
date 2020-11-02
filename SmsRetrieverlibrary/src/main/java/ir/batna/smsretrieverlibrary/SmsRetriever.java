49136ccf1e3e7be93faa6c095bafba0df7864088
bakhtiari <mbwithoutd@gmail.com>
2020-11-02 10:27:48 +0330

    Add Constant class

--- SmsRetrieverlibrary/src/main/java/ir/batna/smsretrieverlibrary/SmsRetriever.java
+++ SmsRetrieverlibrary/src/main/java/ir/batna/smsretrieverlibrary/SmsRetriever.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import ir.batna.smsretrieverlibrary.utils.Constant;
+
 public class SmsRetriever extends BroadcastReceiver {
 
     private static final String TAG = SmsRetriever.class.getName();
@@ -16,9 +18,7 @@ public class SmsRetriever extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
-        String code = intent.getStringExtra("CODE");
-        Log.d(TAG, "Code received: " + code);
+        String code = intent.getStringExtra(Constant.CODE_KEY);
         smsListener.otpReceived(code);
     }
 }
---
+++ SmsRetrieverlibrary/src/main/java/ir/batna/smsretrieverlibrary/utils/Constant.java
@@ -0,0 +1,7 @@
+package ir.batna.smsretrieverlibrary.utils;
+
+public class Constant {
+
+    public static final String CODE_KEY = "code";
+
+}
